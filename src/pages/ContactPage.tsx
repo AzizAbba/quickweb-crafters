8f24e096540f88ff9ee5b4895928cd0dc7cc6b5e
AzizAbba <azizabboud00@gmail.com>
2025-03-31 22:04:50 +0300

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -136,15 +136,6 @@ const ContactPage = () => {
             
             <div className="aspect-w-16 aspect-h-9 max-w-5xl mx-auto">
               <div className="rounded-lg overflow-hidden shadow-lg bg-gray-200 h-[400px] flex items-center justify-center">
-                <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.03606167044!2d-74.25987368715491!3d40.697403441909546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1602859654458!5m2!1sen!2s" 
-                  width="100%" 
-                  height="100%" 
-                  className="border-0"
-                  allowFullScreen 
-                  loading="lazy"
-                  title="Map"
-                ></iframe>
               </div>
             </div>
           </div>
